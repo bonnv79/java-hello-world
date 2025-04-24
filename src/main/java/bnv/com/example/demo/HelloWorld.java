603655f448aea9168227a72b90f1cd7560e02b03
Bon Ngo <ngovanbon16@gmail.com>
2025-04-24 19:16:09 +0700

    update hello world demo

--- src/main/java/bnv/com/example/demo/HelloWorld.java
+++ src/main/java/bnv/com/example/demo/HelloWorld.java
@@ -1,5 +1,7 @@
 package bnv.com.example.demo;
 
+import java.util.Arrays;
+
 public class HelloWorld {
     int myNum = 5;               // Integer (whole number)
     float myFloatNum = 5.99f;    // Floating point number
@@ -18,6 +20,30 @@ public class HelloWorld {
         System.out.println("demo switch");
         checkDay(1);
         checkDay(0);
+
+        System.out.println("===================================");
+        System.out.println("demo while");
+        demoWhile(5);
+
+        System.out.println("===================================");
+        System.out.println("demo For Break");
+        demoForBreak(5);
+
+        System.out.println("===================================");
+        System.out.println("demo For Continue");
+        demoForContinue(5);
+
+        System.out.println("===================================");
+        System.out.println("demo array");
+        demoArray();
+
+        System.out.println("===================================");
+        System.out.println("demo for array");
+        demoForArray();
+
+        System.out.println("===================================");
+        System.out.println("demo Multi-Dimensional Array");
+        demoMultiDimensionalArray();
     }
 
     public static void checkNumber(int x) {
@@ -54,6 +80,72 @@ public class HelloWorld {
                 break;
             default:
                 System.out.println("Looking forward to the Weekend");
+        }
+    }
+
+    public static void demoWhile(int n) {
+        int i = 0;
+        while (i < n) {
+            System.out.println("while: " + i);
+            i++;
+        }
+    }
+
+    public static void demoForBreak(int n) {
+        for (int i = 0; i < n; i++) {
+            if (i == 2) {
+                break;
+            }
+            System.out.println("for break: " + i);
+        }
+    }
+
+    public static void demoForContinue(int n) {
+        for (int i = 0; i < n; i++) {
+            if (i == 2) {
+                continue;
+            }
+            System.out.println("for continue: " + i);
+        }
+    }
+
+    public static void demoArray() {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        cars[0] = "Opel";
+        System.out.println("first item: " + cars[0]);
+        System.out.println(Arrays.toString(cars));
+        System.out.println("length: " + cars.length);
+    }
+
+    public static void demoForArray() {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        System.out.println("Loop Through an Array");
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+
+        System.out.println("Loop Through an Array with For-Each");
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+
+    public static void demoMultiDimensionalArray() {
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+
+        System.out.println("Loop Through a Multi-Dimensional Array");
+        for (int i = 0; i < myNumbers.length; ++i) {
+            for (int j = 0; j < myNumbers[i].length; ++j) {
+                System.out.println("[" + i +  "]" + "[" + j +  "]" + " : " + myNumbers[i][j]);
+            }
+        }
+
+        System.out.println("Loop Through a Multi-Dimensional Array (for-each)");
+        for (int[] row : myNumbers) {
+            for (int i : row) {
+                System.out.println("[" + i +  "]" + " : " + i);
+            }
         }
     }
 }
